6f66a3b402c8cff25d7a029362e854fdb3dbc45e
xue <linluoshengzh@163.com>
2019-01-03 17:39:00 +0800

    mm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Index from './component/login'
+import Login from './component';
+import {HashRouter as Router ,Link,Route,Redirect}  from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+     <Router>
+     <div>
+
+ 
+        <Route path='/login' component={Login}></Route>
+        <Route path='/index' component={Index}></Route>
+        </div>
+     </Router>
+     
+     </div>
     );
   }
 }
---
+++ src/component/login.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+import {
+  Form, Icon, Input, Button,
+} from 'antd';
+
+import '../css/login.css'
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+ export default class Login extends React.Component {
+  // componentDidMount() {
+  //   // To disabled submit button at the beginning.
+  //   this.props.form.validateFields();
+  // }
+
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   this.props.form.validateFields((err, values) => {
+  //     if (!err) {
+  //       console.log('Received values of form: ', values);
+  //     }
+  //   });
+  // }
+
+  render() {
+    // const {
+    //   getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
+    // } = this.props.form;
+
+    // Only show error after a field is touched.
+    // const userNameError = isFieldTouched('userName') && getFieldError('userName');
+    // const passwordError = isFieldTouched('password') && getFieldError('password');
+    return (
+      <Form layout="inline" className='container'>
+        <Form.Item className='item'>
+          
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+         
+        </Form.Item>
+        <Form.Item className='item'>
+         
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+
+        </Form.Item>
+
+        <Form.Item className='item'>
+         
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+
+        </Form.Item>
+        <Form.Item className='item'>
+          <Button
+            type="primary"
+            htmlType="submit"
+           
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
