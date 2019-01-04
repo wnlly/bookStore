5ee7e21490b03048b456a42c66fa7e345f3a2cf5
xue <linluoshengzh@163.com>
2019-01-04 17:07:34 +0800

    register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Index from './component/login'
-import Login from './component';
+import Index from './component/index'
+import Login from './component/login';
+
 import {HashRouter as Router ,Link,Route,Redirect}  from 'react-router-dom'
+import Register from './component/register';
 class App extends Component {
   render() {
     return (
@@ -14,6 +16,7 @@ class App extends Component {
  
         <Route path='/login' component={Login}></Route>
         <Route path='/index' component={Index}></Route>
+        <Route path='/register' component={Register}></Route>
         </div>
      </Router>
      
--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios  from 'axios'
 import {
   Form, Icon, Input, Button,
 } from 'antd';
